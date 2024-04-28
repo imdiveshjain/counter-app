90738accdcb34a29c9c752493ec5734b5ff6935e
Divesh Jain <diveshjain745@gmail.com>
2024-04-28 15:46:22 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+function App() {
+  let [count, setCount] = useState(0);
+
+  function handleCount(value) {
+    setCount(count + value);
+  }
+
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={() => handleCount(1)}>+</button>
+      <button onClick={() => handleCount(-1)}>-</button>
+      <button onClick={() => handleCount(5)}>+5</button>
+      <button onClick={() => handleCount(-5)}>-5</button>
+      <button onClick={() => handleCount(10)}>+10</button>
+      <button onClick={() => handleCount(-10)}>-10</button>
+    </div>
+  );
+}
+
+export default App;
